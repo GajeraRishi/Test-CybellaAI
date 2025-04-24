dba29c32c63f0660ba37784f15a63f69e93d0e66
Gajerarishi <rishigajera9128@gmail.com>
2025-04-24 18:31:45 +1000

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/AI-Cybella/",
+  base:"/AICybella/",
   server: {
     host: "::",
     port: 8080,
